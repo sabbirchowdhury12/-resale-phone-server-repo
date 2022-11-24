9255aa93c6c6597563082fffd6e0878e1f751b93
sabbir <sabbir@gmail.com>
2022-11-24 14:34:25 +0600

    environment setup

--- index.js
+++ index.js
@@ -12,14 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.OhR2T1r8qVQfaaIg}@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+console.log(uri);
 
 app.get('/', async (req, res) => {
     res.send('reseller project is running');
