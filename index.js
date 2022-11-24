be58a2db3bf15ec63039d2dfb8e7c00f43d1234b
sabbir <sabbir@gmail.com>
2022-11-24 15:07:30 +0600

    get category data

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 // const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const { query } = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //midleware
@@ -12,10 +11,29 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.OhR2T1r8qVQfaaIg}@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
+
+async function run() {
+    try {
+
+        const Category = client.db('reSellPhone').collection('category');
+
+        //get all category from db
+        app.get('/category', async (req, res) => {
+            const result = await Category.find({}).toArray();
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
 
 app.get('/', async (req, res) => {
     res.send('reseller project is running');
