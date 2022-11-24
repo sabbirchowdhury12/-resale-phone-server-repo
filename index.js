21bfb5601971ea780b93b6f79dc3fe7ad7397363
sabbir <sabbir@gmail.com>
2022-11-24 14:30:39 +0600

    connect with database

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = "mongodb+srv://<username>:<password>@cluster0.6jo974x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.get('/', async (req, res) => {
     res.send('reseller project is running');
 });
